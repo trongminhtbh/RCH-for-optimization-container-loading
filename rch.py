64762904bdba8a7b6a53b1b5ab1aba2595b1018a
Minh <trongminhtbh@gmail.com>
2022-05-26 19:05:46 +0700

    update sort

--- rch.py
+++ rch.py
@@ -2,12 +2,19 @@ from preprocessing import processBoxes
 from block import Block
 from input import Input
 from utils import displayBoxes
+import random
+
+def sortBoxes(listBoxes):
+    caseNo = random.randint(1,2)
+    listBoxes.sort(key=lambda x: x.taxibility, reverse=True)
+    return listBoxes
 
 def RCH(listBoxes, n):
     for i in range(n):
         print("\n")
         print("<------------Iteration {}-------------->".format(i))
         listBoxes = processBoxes(listBoxes)
+        sortBoxes(listBoxes)
         displayBoxes(listBoxes)
 
 
